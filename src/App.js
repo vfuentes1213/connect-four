a7a4b105de6704dbc685ac5822300bffdbbd766f
Victor Fuentes <vfuentes1213@gmail.com>
2019-08-27 23:15:47 -0600

    Adding modal styles.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import GameScreen from './containers/GameScreen';
 function App() {
   return (
     <div className='App'>
-      {/* <Modal message='Player 1 - Please choose your color:'>
-        <button className='btn'>red</button>
-        <button className='btn'>black</button>
-      </Modal> */}
-      <GameScreen/>
-      </div>
+      <Modal message='Player 1 - Please choose your color:'>
+        <button className='btn btn-danger'>red</button>
+        <button className='btn btn-dark'>black</button>
+      </Modal>
+      <GameScreen />
+    </div>
   );
 }
 
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const modal = (props) =>
-    (<div className = 'Modal'>
+const modal = (props) => (
+  <div className='Modal'>
+    <div className='Modal__backdrop' />
+    <div className='Modal__content'>
       <header className='Modal__header'>{props.message}</header>
-      <div className="Modal__button-box">{props.children}</div>
-      </div>);
+      <div className='Modal__button-box'>{props.children}</div>
+    </div>
+  </div>
+);
 
 export default modal;
